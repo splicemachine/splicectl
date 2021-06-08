4908f399cc5de32d3957d3ae8bf441248b24ef78
riley.mccuen <riley.mccuen@wustl.edu>
2021-06-08 17:25:56 -0500

    added api call, need to find build with semver

--- cmd/changelog.go
+++ cmd/changelog.go
@@ -1,6 +1,10 @@
 package cmd
 
 import (
+	"fmt"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -12,6 +16,14 @@ var changelogCmd = &cobra.Command{
 `,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: output the changelog var...
+		logrus.Info(versionDetail.VersionInfo.Client.SemVer)
+		url := fmt.Sprintf("https://api.github.com/repos/splicemachine/splicectl/releases/tags/%s", versionDetail.VersionInfo.Client.SemVer)
+		client := resty.New()
+		resp, err := client.R().Get(url)
+		if err != nil {
+			logrus.WithError(err).Fatal("request could not be completed due to error, changelog is retrieved through api call to Github that requires internet access")
+		}
+		logrus.Info(string(resp.Body()))
 	},
 }
 
