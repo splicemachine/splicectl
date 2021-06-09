4010c9c8e89497fe3992949ce0a4f8962f36ee9a
riley.mccuen <riley.mccuen@wustl.edu>
2021-06-09 08:42:10 -0500

    Added changelog command

--- cmd/changelog.go
+++ cmd/changelog.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/go-resty/resty/v2"
@@ -16,14 +17,22 @@ var changelogCmd = &cobra.Command{
 `,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: output the changelog var...
-		logrus.Info(versionDetail.VersionInfo.Client.SemVer)
-		url := fmt.Sprintf("https://api.github.com/repos/splicemachine/splicectl/releases/tags/%s", versionDetail.VersionInfo.Client.SemVer)
+		_, semVerNum := ClientSemVer()
+		url := fmt.Sprintf("https://api.github.com/repos/splicemachine/splicectl/releases/tags/%s", semVerNum)
 		client := resty.New()
 		resp, err := client.R().Get(url)
 		if err != nil {
 			logrus.WithError(err).Fatal("request could not be completed due to error, changelog is retrieved through api call to Github that requires internet access")
 		}
-		logrus.Info(string(resp.Body()))
+		jsonMap := make(map[string]interface{})
+		if err := json.Unmarshal(resp.Body(), &jsonMap); err != nil {
+			logrus.WithError(err).Fatal("could not get changelog, recieved error while parsing gh api response")
+		}
+		if _, ok := jsonMap["body"]; !ok {
+			logrus.Fatal("an unexpected response was returned from the Github api, the changelog cannot be found")
+		}
+		// using fmt here instead of logrus/log in order to get rid of leading [INFO] or similar header
+		fmt.Println(jsonMap["body"])
 	},
 }
 
